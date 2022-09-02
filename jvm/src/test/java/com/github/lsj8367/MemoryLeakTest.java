b28fe4088a2595916c1efcc486aa0638b357061a
lsj8367 <lsj8367@naver.com>
2022-09-02 23:48:17 +0900

    :sparkles: memory out of memory exception test

---
+++ jvm/src/test/java/com/github/lsj8367/MemoryLeakTest.java
@@ -0,0 +1,41 @@
+package com.github.lsj8367;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class MemoryLeakTest {
+
+
+    @Test
+    void test() {
+        Leak leak = new Leak();
+        for (int i = 0; i < 90000000; i++) {
+            leak.addList(i);
+            leak.removeStr(i);
+        }
+    }
+
+    @Test
+    void a() {
+        String a = "a";
+        String b = a;
+        b = null;
+        System.out.println(a);
+    }
+
+
+    private static class Leak {
+
+        List<String> list = new ArrayList<>();
+
+        private void addList(int a) {
+            list.add("testetsetsetset" + a);
+        }
+
+        private void removeStr(int i) {
+            Object obj = list.get(i);
+            obj = null;
+        }
+    }
+}
