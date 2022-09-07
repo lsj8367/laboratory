fed71b78043b17f9e40ba9c3f058e86cb6dbb8df
lsj8367 <lsj8367@naver.com>
2022-09-07 23:44:42 +0900

    :sparkles: check, uncheck exception 차이 구현

---
+++ effective-java/src/main/java/com/github/lsj8367/item2/exception/ExceptionExample.java
@@ -0,0 +1,35 @@
+package com.github.lsj8367.item2.exception;
+
+import java.io.IOException;
+
+public class ExceptionExample {
+
+    private void nullPointerException() throws NullPointerException {
+        System.out.println();
+    }
+
+    private void illegalArgumentException() throws IllegalArgumentException {
+        System.out.println();
+    }
+
+    private void runtimeException() throws RuntimeException {
+        System.out.println();
+    }
+
+    private void ioException() throws IOException {
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ExceptionExample example = new ExceptionExample();
+        try {
+            example.ioException();
+        } catch (IOException e) {
+
+        }
+        example.runtimeException();
+        example.illegalArgumentException();
+        example.nullPointerException();
+
+    }
+}
