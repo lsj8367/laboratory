b429ef9d2c98518fbadabe726e9565b78e28b5ca
lsj8367 <lsj8367@naver.com>
2022-09-17 13:36:50 +0900

    :sparkles: item9 이중 예외 try-with-resources 추가

--- effective-java/src/main/java/com/github/lsj8367/item9/TryException.java
+++ effective-java/src/main/java/com/github/lsj8367/item9/TryException.java
@@ -1,17 +1,26 @@
 package com.github.lsj8367.item9;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class TryException {
 
+    // 이중예외중 한개는 무시되는 일반 try-finally
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new ExceptionReader(new InputStreamReader(System.in));
+//        try {
+//            br.readLine();
+//        } finally {
+//            br.close();
+//        }
+//    }
+
+    // 두 예외가 전부 보여지는 try-with-resources
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new ExceptionReader(new InputStreamReader(System.in));
-        try {
+        try(BufferedReader br = new ExceptionReader(new InputStreamReader(System.in))) {
             br.readLine();
-        } finally {
-            br.close();
         }
     }
 }
