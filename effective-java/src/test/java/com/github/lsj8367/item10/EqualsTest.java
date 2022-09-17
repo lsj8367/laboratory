2b18b2e259897dca937ddb6c34ebea37c6e6eb61
lsj8367 <lsj8367@naver.com>
2022-09-17 14:56:36 +0900

    :white_check_mark: linkedList equals override test

--- effective-java/src/test/java/com/github/lsj8367/item10/EqualsTest.java
+++ effective-java/src/test/java/com/github/lsj8367/item10/EqualsTest.java
@@ -1,16 +1,18 @@
 package com.github.lsj8367.item10;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import java.util.LinkedList;
 import java.util.List;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class EqualsTest {
+class EqualsTest {
 
     @Test
-    void test() {
-        final List<Integer> list1 = List.of(1, 2, 3);
-        final List<Integer> list2 = List.of(1, 2, 3);
+    void Should_equalTrue_When_linkedListIsOverride() {
+        final List<Integer> list1 = new LinkedList<>();
+        list1.add(1);
+        final List<Integer> list2 = new LinkedList<>();
+        list2.add(1);
 
         System.out.println(list1.equals(list2));
 
