0ea9ef4b27effffb40574051c1917ea644fcca43
lsj8367 <lsj8367@naver.com>
2022-09-26 23:44:37 +0900

    :sparkles: 공변 반환 타이핑

---
+++ effective-java/src/main/java/com/github/lsj8367/item13/ChildTest.java
@@ -0,0 +1,16 @@
+package com.github.lsj8367.item13;
+
+public class ChildTest extends Test {
+
+//    @Override
+//    public Test method1() {
+//        return null;
+//    }
+
+
+    @Override
+    public ChildTest method1() {
+        return null;
+    }
+
+}
---
+++ effective-java/src/main/java/com/github/lsj8367/item13/Test.java
@@ -0,0 +1,9 @@
+package com.github.lsj8367.item13;
+
+public class Test {
+
+    public Test method1() {
+        return new Test();
+    }
+
+}
