8623f15c75c41399535905643611a9aba32d6857
lsj8367 <lsj8367@naver.com>
2022-10-16 01:31:56 +0900

    :sparkles: 와일드 카드 타입인 경우 값 추가 시 컴파일 예외

---
+++ effective-java/src/main/java/com/github/lsj8367/item26/Demo.java
@@ -0,0 +1,19 @@
+package com.github.lsj8367.item26;
+
+import java.util.List;
+
+public class Demo {
+
+    public static void main(String[] args) {
+//        Demo demo = new Demo();
+//        List<String> list = new ArrayList<>();
+//        list.add("aaa");
+//        demo.add(list);
+    }
+
+
+    public void add(List<?> list) {
+//        list.add(1);
+    }
+
+}
