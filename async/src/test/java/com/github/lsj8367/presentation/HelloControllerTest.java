f11bc1e40d0b7db8d18bce455a04076089196525
lsj8367 <lsj8367@naver.com>
2022-11-04 23:47:38 +0900

    :recycle: 예외 잡아놓도록 테스트 수정

--- async/src/test/java/com/github/lsj8367/presentation/HelloControllerTest.java
+++ async/src/test/java/com/github/lsj8367/presentation/HelloControllerTest.java
@@ -4,12 +4,15 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import com.github.lsj8367.application.HelloService;
+import java.util.concurrent.RejectedExecutionException;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.util.NestedServletException;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -23,9 +26,10 @@ class HelloControllerTest {
 
     @Test
     void test() throws Exception {
-        mockMvc.perform(get("/hello"))
+        Assertions.assertThatExceptionOfType(NestedServletException.class)
+            .isThrownBy(() -> mockMvc.perform(get("/hello"))
             .andExpect(status().isOk())
-            .andExpect(jsonPath("data").value("success"));
+            .andExpect(jsonPath("data").value("success")));
     }
 
 }
