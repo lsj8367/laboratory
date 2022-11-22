a32958e73f67ca6697e7dea605fef9c5b9d23f8f
lsj8367 <lsj8367@naver.com>
2022-11-22 23:31:51 +0900

    :sparkles: main class init

---
+++ effective-java/src/main/java/com/github/lsj8367/EffectiveJavaApplication.java
@@ -0,0 +1,13 @@
+package com.github.lsj8367;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectiveJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EffectiveJavaApplication.class);
+    }
+
+}
