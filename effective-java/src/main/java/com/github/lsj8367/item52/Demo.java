427f2cabdd1db02ff1e550a45d0e7e1bf22febf2
lsj8367 <lsj8367@naver.com>
2022-11-22 23:18:05 +0900

    :sparkles: 52장 다중정의 해소 알고리즘

---
+++ effective-java/src/main/java/com/github/lsj8367/item52/Demo.java
@@ -0,0 +1,15 @@
+package com.github.lsj8367.item52;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        //다중정의 해소 알고리즘
+//        new Thread(System.out::println).start();
+//        ExecutorService exec = Executors.newCachedThreadPool();
+//        exec.submit(System.out::println);
+    }
+
+}
