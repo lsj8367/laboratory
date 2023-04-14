8ea0ce1a2bf4131dd7e020215d39f8c69a3e9435
lsj8367 <lsj8367@naver.com>
2023-04-15 02:18:59 +0900

    :sparkles: cloud 설정

---
+++ cloud-config/src/test/java/com/gitgub/lsj8367/cloudconfig/CloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitgub.lsj8367.cloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
