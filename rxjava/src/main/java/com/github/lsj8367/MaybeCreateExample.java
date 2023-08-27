ca3df5b789da1c462f6d38d0c172ae3390e33baf
lsj8367 <lsj8367@naver.com>
2023-08-27 23:31:24 +0900

    feat: add maybe example

--- rxjava/src/main/java/com/github/lsj8367/MaybeCreateExample.java
+++ rxjava/src/main/java/com/github/lsj8367/MaybeCreateExample.java
@@ -1,2 +1,37 @@
-package com.github.lsj8367;public class MaybeCreateExample {
+package com.github.lsj8367;
+
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Maybe;
+import io.reactivex.rxjava3.core.MaybeEmitter;
+import io.reactivex.rxjava3.core.MaybeObserver;
+import io.reactivex.rxjava3.disposables.Disposable;
+
+public class MaybeCreateExample {
+    public static void main(String[] args) {
+        final Maybe<String> maybe = Maybe.create(MaybeEmitter::onComplete);
+
+        maybe.subscribe(new MaybeObserver<>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                System.out.println("on subscribe");
+            }
+
+            @Override
+            public void onSuccess(@NonNull String s) {
+                System.out.println("on success");
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                System.out.println("on error");
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("on complete");
+            }
+        });
+
+
+    }
 }
--- rxjava/src/main/java/com/github/lsj8367/MaybeJustExample.java
+++ rxjava/src/main/java/com/github/lsj8367/MaybeJustExample.java
@@ -1,2 +1,16 @@
-package com.github.lsj8367;public class MaybeJustExample {
+package com.github.lsj8367;
+
+import io.reactivex.rxjava3.core.Maybe;
+
+import java.time.LocalDateTime;
+
+public class MaybeJustExample {
+    public static void main(String[] args) {
+        Maybe.just(LocalDateTime.now())
+                .subscribe(
+                        data -> System.out.println("현재시각 : " + data),
+                        error -> System.out.println("error!!!" + error),
+                        () -> System.out.println("complete!")
+                );
+    }
 }
